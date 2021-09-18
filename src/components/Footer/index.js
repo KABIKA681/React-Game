df3cf9f0c336508d4b95f7ba298141460abf35d3
KABIKA681 <pascalkabikampan@gmail.com>
2021-09-19 00:21:46 +0200

    second commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <div className="footer-container">
-        <p>Project done by Pascal Kabika Mp 2021</p>
+        <p>Project done by Mpangirwa 2021</p>
         <p>All Icons were found on iconFinder.com</p>
       </div>
     </footer>
